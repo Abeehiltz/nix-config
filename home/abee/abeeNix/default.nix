946436647ff16d5436b1163481c2fb23daeef32f
abee <36138393+Abeehiltz@users.noreply.github.com>
2025-10-09 20:48:58 +0200

    add ssh-agent start

--- home/abee/abeeNix/default.nix
+++ home/abee/abeeNix/default.nix
@@ -7,6 +7,9 @@
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
   
+  # setup SSH agent
+  programs.ssh.startAgent = true;
+
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
   # introduces backwards incompatible changes.

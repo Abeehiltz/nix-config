e34c867b3b68a95d169e274ba521bb793d7df4c9
abee <36138393+Abeehiltz@users.noreply.github.com>
2025-10-13 18:11:20 +0200

    monitor config hyprland

---
+++ modules/home-manager/desktop/hyprland/monitor.nix
@@ -0,0 +1,12 @@
+{...}:
+{
+  wayland.windowManager.hyprland.settings = {
+    monitor = [
+      # 2K monitor on the left
+      "DP-2,2560x1440@120,auto-left,1"
+
+      # 4K monitor OLED
+      "DP-3,3840x2160@240,0x0,1.5"
+    ];
+  };
+}

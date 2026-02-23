0097bbd0525387139a4f3b5e210d70cc0197080e
abee <36138393+Abeehiltz@users.noreply.github.com>
2026-02-23 12:01:23 +0100

    add extra config steam

--- modules/nixos/programs/steam/default.nix
+++ modules/nixos/programs/steam/default.nix
@@ -1,8 +1,13 @@
-{ ... }:
+{ pkgs, ... }:
 {
   # Steam gaming platform configuration
   programs.steam = {
     enable = true;
     remotePlay.openFirewall = true;
+    dedicatedServer.openFirewall = true;
+    localNetworkGameTransfers.openFirewall = true;
+    extraCompatPackages = with pkgs; [
+      proton-ge-bin
+    ];
   };
 }

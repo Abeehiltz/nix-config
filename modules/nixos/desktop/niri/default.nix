42a629a51500cb60d1013c99e9e7b7fcdac19e8b
abee <36138393+Abeehiltz@users.noreply.github.com>
2026-03-24 12:46:47 +0100

    comment extra systemd and xdg portal fixes after sddm seems to have fixed them

--- modules/nixos/desktop/niri/default.nix
+++ modules/nixos/desktop/niri/default.nix
@@ -19,19 +19,20 @@ in {
   };
 
   # repeated with home-manager config to fix https://github.com/sodiboo/niri-flake/issues/509
-  xdg.portal = {
-    enable = true;
-    xdgOpenUsePortal = true;
-    extraPortals = [ pkgs.xdg-desktop-portal-wlr pkgs.xdg-desktop-portal-gtk ];
-    config.niri = { 
-      default = [ "gtk" "gnome" ];
-      "org.freedesktop.impl.portal.Access" = "gtk";
-      "org.freedesktop.impl.portal.Notification" = "gtk";
-      "org.freedesktop.impl.portal.Secret" = "gnome-keyring";
-      "org.freedesktop.impl.portal.FileChooser" = "gtk";
-    };
-    config.common.default = [ "gtk" "gnome" ];
-  };
+  # Update 2026-03-24: commented after moving to SDDM since it seems to be fixed now. To remove in a few days.
+#  xdg.portal = {
+#    enable = true;
+#    xdgOpenUsePortal = true;
+#    extraPortals = [ pkgs.xdg-desktop-portal-wlr pkgs.xdg-desktop-portal-gtk ];
+#    config.niri = { 
+#      default = [ "gtk" "gnome" ];
+#      "org.freedesktop.impl.portal.Access" = "gtk";
+#      "org.freedesktop.impl.portal.Notification" = "gtk";
+#      "org.freedesktop.impl.portal.Secret" = "gnome-keyring";
+#      "org.freedesktop.impl.portal.FileChooser" = "gtk";
+#    };
+#    config.common.default = [ "gtk" "gnome" ];
+#  };
 
   environment.systemPackages = with pkgs; [
     kitty
@@ -40,27 +41,27 @@ in {
   ];
 
   # Enable security services
-  services.gnome.gnome-keyring.enable = true;
-  security.polkit.enable = true;
-  security.pam.services = {
-    gdm.enableGnomeKeyring = true;
-  };
-
-  systemd.user.services.xdg-desktop-portal = {
-    after = [ "xdg-desktop-autostart.target" ];
-  };
-
-  systemd.user.services.xdg-desktop-portal-gtk = {
-    after = [ "xdg-desktop-autostart.target" ];
-  };
-
-  systemd.user.services.xdg-desktop-portal-gnome = {
-    after = [ "xdg-desktop-autostart.target" ];
-  };
-
-  systemd.user.services.niri-flake-polkit = {
-    after = [ "xdg-desktop-autostart.target" ];
-  };
+#  services.gnome.gnome-keyring.enable = true;
+#  security.polkit.enable = true;
+#  security.pam.services = {
+#    gdm.enableGnomeKeyring = true;
+#  };
+#
+#  systemd.user.services.xdg-desktop-portal = {
+#    after = [ "xdg-desktop-autostart.target" ];
+#  };
+#
+#  systemd.user.services.xdg-desktop-portal-gtk = {
+#    after = [ "xdg-desktop-autostart.target" ];
+#  };
+#
+#  systemd.user.services.xdg-desktop-portal-gnome = {
+#    after = [ "xdg-desktop-autostart.target" ];
+#  };
+#
+#  systemd.user.services.niri-flake-polkit = {
+#    after = [ "xdg-desktop-autostart.target" ];
+#  };
 
   # Configure SDDM
   services.displayManager = {

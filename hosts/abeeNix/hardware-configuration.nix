c64987bd6c6855856099df5bf44528e979f17767
abee <36138393+Abeehiltz@users.noreply.github.com>
2026-03-24 12:46:27 +0100

    add irqpol kernel Params to fix irq error

--- hosts/abeeNix/hardware-configuration.nix
+++ hosts/abeeNix/hardware-configuration.nix
@@ -11,6 +11,7 @@
   boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usbhid" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
+  boot.kernelParams = [ "irqpoll" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =

1183db93b664895748ee4ca186362c129cca894d
abee <36138393+Abeehiltz@users.noreply.github.com>
2026-06-09 18:03:09 +0200

    setup raphael-rs icon

--- pkgs/raphael-rs/default.nix
+++ pkgs/raphael-rs/default.nix
@@ -20,14 +20,27 @@ rustPlatform.buildRustPackage {
   ];
 
   postInstall = ''
-    for f in $out/bin/*; do
-      wrapProgram "$f" \
-        --prefix LD_LIBRARY_PATH : ${lib.makeLibraryPath [ libGL wayland libxkbcommon ]}
-    done
+    wrapProgram $out/bin/raphael-xiv \
+      --prefix LD_LIBRARY_PATH : ${lib.makeLibraryPath [ libGL wayland libxkbcommon ]}
+  
+    mkdir -p $out/share/applications
+    mkdir -p $out/share/icons/hicolor/64x64/apps
+    cp ${src}/assets/favicon-64x64.png $out/share/icons/hicolor/64x64/apps/raphael-xiv.png
+  
+    cat > $out/share/applications/raphael-xiv.desktop << EOF
+    [Desktop Entry]
+    Name=Raphael XIV
+    Comment=FFXIV Crafting Macro Solver
+    Exec=$out/bin/raphael-xiv
+    Icon=raphael-xiv
+    Terminal=false
+    Type=Application
+    Categories=Game;Utility;
+    EOF
   '';
 
   meta = {
-    description = "FFXIV crafting macro solver (GUI)";
+    description = "FFXIV crafting macro solver";
     homepage = "https://github.com/KonaeAkira/raphael-rs";
     platforms = lib.platforms.linux;
   };

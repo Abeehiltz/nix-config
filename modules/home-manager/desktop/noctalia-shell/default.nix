c7cffd3bdc6256e077a1f3397e261a03f7c2a617
abee <36138393+Abeehiltz@users.noreply.github.com>
2026-06-11 22:50:27 +0200

    update

--- modules/home-manager/desktop/noctalia-shell/default.nix
+++ modules/home-manager/desktop/noctalia-shell/default.nix
@@ -2,7 +2,7 @@
 {
   imports = [ inputs.noctalia.homeModules.default ];
   
-  programs.noctalia-shell = {
+  programs.noctalia = {
     enable = true;
 
     settings = {
@@ -425,76 +425,6 @@
         sitOnFrame = false;
         showFrameIndicator = true;
       };
-      network = {
-        wifiEnabled = false;
-        airplaneModeEnabled = false;
-        bluetoothRssiPollingEnabled = false;
-        bluetoothRssiPollIntervalMs = 60000;
-        wifiDetailsViewMode = "grid";
-        bluetoothDetailsViewMode = "grid";
-        bluetoothHideUnnamedDevices = false;
-        disableDiscoverability = false;
-      };
-      sessionMenu = {
-        enableCountdown = true;
-        countdownDuration = 10000;
-        position = "center";
-        showHeader = true;
-        showKeybinds = true;
-        largeButtonsStyle = true;
-        largeButtonsLayout = "single-row";
-        powerOptions = [
-          {
-            action = "lock";
-            command = "";
-            countdownEnabled = true;
-            enabled = true;
-            keybind = "1";
-          }
-          {
-            action = "suspend";
-            command = "";
-            countdownEnabled = true;
-            enabled = true;
-            keybind = "2";
-          }
-          {
-            action = "hibernate";
-            command = "";
-            countdownEnabled = true;
-            enabled = true;
-            keybind = "3";
-          }
-          {
-            action = "reboot";
-            command = "";
-            countdownEnabled = true;
-            enabled = true;
-            keybind = "4";
-          }
-          {
-            action = "logout";
-            command = "";
-            countdownEnabled = true;
-            enabled = true;
-            keybind = "5";
-          }
-          {
-            action = "shutdown";
-            command = "";
-            countdownEnabled = true;
-            enabled = true;
-            keybind = "6";
-          }
-          {
-            action = "rebootToUefi";
-            command = "";
-            countdownEnabled = true;
-            enabled = true;
-            keybind = "7";
-          }
-        ];
-      };
       notifications = {
         enabled = true;
         enableMarkdown = false;
@@ -615,26 +545,8 @@
         manualSunrise = "06:30";
         manualSunset = "18:30";
       };
-      hooks = {
-        enabled = false;
-        wallpaperChange = "";
-        darkModeChange = "";
-        screenLock = "";
-        screenUnlock = "";
-        performanceModeEnabled = "";
-        performanceModeDisabled = "";
-        startup = "";
-        session = "";
-      };
       plugins = {
         autoUpdate = false;
-      };
-      desktopWidgets = {
-        enabled = false;
-        gridSnap = false;
-        monitorWidgets = [
-    
-        ];
       };
 
     };

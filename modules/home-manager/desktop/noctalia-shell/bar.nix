1bf3a94d74e21af4ea9add29452f34c75b1ff460
abee <36138393+Abeehiltz@users.noreply.github.com>
2026-06-25 21:50:17 +0200

    noctalia v5 config

--- modules/home-manager/desktop/noctalia-shell/bar.nix
+++ modules/home-manager/desktop/noctalia-shell/bar.nix
@@ -9,9 +9,9 @@
       margin_edge = 5;
       #radius_top_left = 0;
       #radius_top_right = 0;
-      start = [ "launcher" "audio_visualizer" ];
+      start = [ "clock" "launcher" "audio_visualizer" ];
       center = [ "workspaces" ];
-      end = [ "tray" "notifications" "volume" "clock" "control-center" "session" ];
+      end = [ "tray" "notifications" "volume" "control-center" "session" ];
     };
 
   };
--- modules/home-manager/desktop/noctalia-shell/theming.nix
+++ modules/home-manager/desktop/noctalia-shell/theming.nix
@@ -9,7 +9,7 @@
       enable_builtin_templates = true;
       enable_community_templates = true;
       
-      builtin_ids = [ "niri" "gtk3" "gt4" "qt" "kitty" "starship"];
+      builtin_ids = [ "niri" "gtk3" "gt4" "qt" "kitty" ];
       community_ids = ["zen-browser" "neovim" "obsidian" "discord" "steam" "yazi"];
     };
   };
